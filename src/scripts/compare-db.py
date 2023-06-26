4e68001278f49b76984dcff33eb4abfda1434d19
IcyComet <s2062730@ed.ac.uk>
2023-06-26 17:42:09 +0100

    improved comparison of .db files

--- src/scripts/compare-db.py
+++ src/scripts/compare-db.py
@@ -4,9 +4,11 @@ import argparse
 from ase.io import read
 
 def main(dbFileA: str, dbFileB: str):
-    dataA, dataB = read(dbFileA), read(dbFileB)
-    print(f"{dbFileA} contains {len(dataA)} atoms objects")
-    print(f"{dbFileB} contains {len(dataB)} atoms objects")
+    atomListA, atomListB = read(dbFileA, index=slice(None)), read(dbFileB, index=slice(None))
+    print(f"{dbFileA} contains {len(atomListA)} atoms objects")
+    print(f"{dbFileB} contains {len(atomListB)} atoms objects")
+    print(f"number of atoms in final Atoms object of {dbFileA}: {len(atomListA[-1])}")
+    print(f"number of atoms in final Atoms object of {dbFileB}: {len(atomListB[-1])}")
     return
 
 if __name__ == "__main__":
