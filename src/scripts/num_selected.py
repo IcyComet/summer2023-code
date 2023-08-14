9c144fa31d6244f4bb91fd9c421fbb24325c15c1
IcyComet <s2062730@ed.ac.uk>
2023-08-14 23:24:46 +0100

    count how many SOAP vectors will be selected

---
+++ src/scripts/num_selected.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import argparse
+import numpy as np
+from glob import glob
+from os.path import isdir, isfile
+
+def main(pathstring: str, distance: float, nstructures: int):
+    files = glob("**/*-lam.npy", root_dir=pathstring, recursive=True) if isdir(pathstring) else [pathstring]
+    assert(all(map(isfile, files)))
+    print(f"Number of files found: {len(files)}")
+    if nstructures is not None:
+        count = nstructures * len(files)
+    else:
+        count = 0
+        for file in files:
+            distances = np.load(file)
+            count += np.count_nonzero(distances >= distance)
+    print(f"Number of structures selected is: {count}")
+    return
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("pathstring", type=str, default=".")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-d", "--distance", type=float)
+    group.add_argument("-n", "--nstructures", type=int)
+    args = parser.parse_args()
+    main(args.pathstring, args.distance, args.nstructures)
